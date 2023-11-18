265f77f7486b9bb05a041e06833e2a1db9788aa9
Ujimatsu Chiya <2428678363@qq.com>
2023-11-18 23:41:33 +0800

    更新setup

--- setup.py
+++ setup.py
@@ -1,28 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(
-    name='Sylveon',
-    version='0.0.0',
-    license='LGPL-3.0',
-    author='Ujimatsu Chiya',
-    author_email='ujimatsuchiya016@gmail.com',
-    maintainer='Ujimatsu Chiya',
-    maintainer_email='ujimatsuchiya016@gmail.com',
-    description='A random number generator on algorithmic questions.',
-    long_description='A longer description of your project',
-    long_description_content_type='text/markdown',
-    url='https://github.com/Ujimatsu-Chiya/Sylveon',
-    packages=find_packages(),
-    install_requires=[
-
-    ],
-    classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.10'
-    ],
-    keywords='python algorithm icpc acm oi'
-)
+setup()
