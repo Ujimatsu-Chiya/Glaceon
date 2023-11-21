68c9f6cf948bf048483b5a9d4f3fef1ecbcdb991
Ujimatsu Chiya <2428678363@qq.com>
2023-11-21 12:58:59 +0800

    完成迷宫生成器的编写，添加了Kruskal算法和Prim算法。

--- Sylveon/__init__.py
+++ Sylveon/__init__.py
@@ -1,11 +1,13 @@
 from .array_generator import ArrayGen
 from .io_file import IOFile
 from .graph_generator import GraphGen
+from .maze_generator import MazeGen
 from .string_generator import StringGen
 from .graph import Graph
 from .utils.rand import *
 
 from random import randint, uniform, random, choices, sample
 
-class Gen(ArrayGen, GraphGen, StringGen):
+
+class Gen(ArrayGen, GraphGen, StringGen, MazeGen):
     pass
--- Sylveon/maze_generator.py
+++ Sylveon/maze_generator.py
@@ -3,7 +3,19 @@ import random
 
 class MazeGen:
     @staticmethod
-    def recursive_backtracking_core(n, m, **kwargs):
+    def __gen_recursive_backtracking_core(n, m, **kwargs):
+        """
+        Generate a maze using the Recursive Backtracking algorithm.
+
+        Parameters:
+            - `n` (int): Number of rows in the maze.
+            - `m` (int): Number of columns in the maze.
+            - `**kwargs`: Additional keyword arguments.
+                - `shuffle_dir` (bool): Whether to shuffle the direction order (default is True).
+
+        Returns:
+            - list of lists: The generated maze represented as a 2D matrix.
+        """
         maze = [[1 for _ in range(m)] for _ in range(n)]
         shuffle_dir = kwargs.get("shuffle_dir", True)
 
@@ -26,7 +38,18 @@ class MazeGen:
         return maze
 
     @staticmethod
-    def recursive_division_core(n, m, **kwargs):
+    def __gen_recursive_division_core(n, m, **kwargs):
+        """
+        Generate a maze using the Recursive Division algorithm.
+
+        Parameters:
+            - `n` (int): Number of rows in the maze.
+            - `m` (int): Number of columns in the maze.
+            - `**kwargs`: Additional keyword arguments.
+
+        Returns:
+            - list of lists: The generated maze represented as a 2D matrix.
+        """
         maze = [[1 for _ in range(m)] for _ in range(n)]
 
         def divide(xa, ya, xb, yb):
@@ -55,3 +78,211 @@ class MazeGen:
             for j in range(ey + 1):
                 maze[i * 2][j * 2] = 0
         return maze
+
+    @staticmethod
+    def __gen_prim_core(n, m, **kwargs):
+        """
+        Generate a maze using the Prim's algorithm.
+
+        Parameters:
+            - `n` (int): Number of rows in the maze.
+            - `m` (int): Number of columns in the maze.
+            - `**kwargs`: Additional keyword arguments.
+                - `shuffle_dir` (bool): Whether to shuffle the direction order (default is True).
+
+        Returns:
+            - list of lists: The generated maze represented as a 2D matrix.
+        """
+        maze = [[1 for _ in range(m)] for _ in range(n)]
+
+        def is_valid(x, y):
+            return 0 <= x < n and 0 <= y < m and maze[x][y] == 1
+
+        sx, sy = random.randint(0, (n - 1) // 2), random.randint(0, (m - 1) // 2)
+        maze[sx][sy] = 0
+        dir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        shuffle_dir = kwargs.get("shuffle_dir", True)
+        ls = [(sx, sy)]
+        while len(ls) > 0:
+            pos = random.randint(0, len(ls) - 1)
+            x, y = ls[pos]
+            ls[pos] = ls[-1]
+            ls.pop()
+            if shuffle_dir:
+                random.shuffle(dir)
+            for dx, dy in dir:
+                nx, ny = x + dx, y + dy
+                if is_valid(nx * 2, ny * 2):
+                    maze[nx * 2][ny * 2] = maze[x * 2 + dx][y * 2 + dy] = 0
+                    ls.append((nx, ny))
+
+        return maze
+
+    @staticmethod
+    def __gen_kruskal_core(n, m, **kwargs):
+        """
+        Generate a maze using the Kruskal's algorithm.
+
+        Parameters:
+            - `n` (int): Number of rows in the maze.
+            - `m` (int): Number of columns in the maze.
+            - `**kwargs`: Additional keyword arguments.
+
+        Returns:
+            - list of lists: The generated maze represented as a 2D matrix.
+        """
+        maze = [[1 for _ in range(m)] for _ in range(n)]
+        ex, ey = (n - 1) // 2, (m - 1) // 2
+        fa = [[(i, j) for j in range(ey + 1)] for i in range(ex + 1)]
+
+        def find(x, y):
+            if fa[x][y] == (x, y):
+                return fa[x][y]
+            else:
+                fa[x][y] = find(*fa[x][y])
+                return fa[x][y]
+
+        def merge(xa, ya, xb, yb):
+            ta, tb = find(xa, ya), find(xb, yb)
+            fa[ta[0]][ta[1]] = tb
+
+        edges = []
+        for i in range(ex + 1):
+            for j in range(ey + 1):
+                if i < ex:
+                    edges.append((i, j, 0))
+                if j < ey:
+                    edges.append((i, j, 1))
+                maze[i * 2][j * 2] = 0
+        random.shuffle(edges)
+        for x, y, d in edges:
+            if d == 0 and find(x, y) != find(x + 1, y):
+                merge(x, y, x + 1, y)
+                maze[x * 2 + 1][y * 2] = 0
+            elif d == 1 and find(x, y) != find(x, y + 1):
+                merge(x, y, x, y + 1)
+                maze[x * 2][y * 2 + 1] = 0
+        return maze
+
+    @staticmethod
+    def __maze_gen_aux(n, m, maze_gen_func, **kwargs):
+        """
+        Auxiliary function for generating a maze with additional features.
+
+        Parameters:
+            - `n` (int): Number of rows in the maze.
+            - `m` (int): Number of columns in the maze.
+            - `maze_gen_func`: The core maze generation function.
+            - `**kwargs`: Additional keyword arguments.
+                - `block_to_empty_rate` (float): Probability to convert a block to an empty cell.
+                - `empty_to_block_rate` (float): Probability to convert an empty cell to a block.
+                - `start_pos` (tuple): Starting position in the maze.
+                - `end_pos` (tuple): Ending position in the maze.
+                - `empty` (str): Representation of an empty cell in the maze.
+                - `block` (str): Representation of a block cell in the maze.
+                - `start_mark` (str): Representation of the starting position.
+                - `end_mark` (str): Representation of the ending position.
+                - `shuffle_dir` (bool): Whether to shuffle the direction order (default is True).
+
+        Returns:
+            - str: The generated maze represented as a string.
+        """
+        block_to_empty_rate = kwargs.get("block_to_empty_rate", 0)
+        empty_to_block_rate = kwargs.get("empty_to_block_rate", 0)
+        start_pos = kwargs.get("start_pos", None)
+        end_pos = kwargs.get("end_pos", None)
+        empty = kwargs.get("empty", "0")
+        block = kwargs.get("block", "1")
+        start_mark = kwargs.get("start_mark", empty)
+        end_mark = kwargs.get("end_mark", empty)
+        maze = maze_gen_func(n, m)
+        if start_pos is not None:
+            maze[start_pos[0]][start_pos[1]] = start_mark
+        if end_pos is not None:
+            maze[end_pos[0]][end_pos[1]] = end_mark
+
+        if n % 2 == 0:
+            for j in range(0, m, 2):
+                if maze[-2][j] == 0 and random.random() < block_to_empty_rate:
+                    maze[-1][j] = 0
+        if m % 2 == 0:
+            for i in range(0, n, 2):
+                if maze[i][-2] == 0 and random.random() < block_to_empty_rate:
+                    maze[i][-1] = 0
+        for i in range(n):
+            for j in range(m):
+                if maze[i][j] == 1 and random.random() < block_to_empty_rate:
+                    maze[i][j] = 0
+                elif maze[i][j] == 0 and random.random() < empty_to_block_rate:
+                    maze[i][j] = 1
+        for i in range(n):
+            for j in range(m):
+                if (i, j) == start_pos:
+                    maze[i][j] = start_mark
+                elif (i, j) == end_pos:
+                    maze[i][j] = end_mark
+                elif maze[i][j] == 0:
+                    maze[i][j] = empty
+                else:
+                    maze[i][j] = block
+        return "\n".join("".join(ch for ch in raw) for raw in maze)
+
+    @staticmethod
+    def gen_recursive_backtracking_maze(n, m, **kwargs):
+        """
+        Generate a maze using the Recursive Backtracking algorithm with additional features.
+
+        Parameters:
+            - `n` (int): Number of rows in the maze.
+            - `m` (int): Number of columns in the maze.
+            - `**kwargs`: Additional keyword arguments.
+
+        Returns:
+            - str: The generated maze represented as a string.
+        """
+        return MazeGen.__maze_gen_aux(n, m, MazeGen.__gen_recursive_backtracking_core, **kwargs)
+
+    @staticmethod
+    def gen_recursive_division_maze(n, m, **kwargs):
+        """
+        Generate a maze using the Recursive Division algorithm with additional features.
+
+        Parameters:
+            - `n` (int): Number of rows in the maze.
+            - `m` (int): Number of columns in the maze.
+            - `**kwargs`: Additional keyword arguments.
+
+        Returns:
+            - str: The generated maze represented as a string.
+        """
+        return MazeGen.__maze_gen_aux(n, m, MazeGen.__gen_recursive_division_core, **kwargs)
+
+    @staticmethod
+    def gen_prim_maze(n, m, **kwargs):
+        """
+        Generate a maze using the Prim's algorithm with additional features.
+
+        Parameters:
+            - `n` (int): Number of rows in the maze.
+            - `m` (int): Number of columns in the maze.
+            - `**kwargs`: Additional keyword arguments.
+
+        Returns:
+            - str: The generated maze represented as a string.
+        """
+        return MazeGen.__maze_gen_aux(n, m, MazeGen.__gen_prim_core, **kwargs)
+
+    @staticmethod
+    def gen_kruskal_maze(n, m, **kwargs):
+        """
+        Generate a maze using the Kruskal's algorithm with additional features.
+
+        Parameters:
+            - `n` (int): Number of rows in the maze.
+            - `m` (int): Number of columns in the maze.
+            - `**kwargs`: Additional keyword arguments.
+
+        Returns:
+            - str: The generated maze represented as a string.
+        """
+        return MazeGen.__maze_gen_aux(n, m, MazeGen.__gen_kruskal_core, **kwargs)
